6df8b08fe0a528d9e0a9d99503fd797a7d132349
Joanice Oliveira <jhoaniceoliveira@gmail.com>
2024-07-29 22:22:45 -0300

    add - resolved exercise #1

---
+++ src/main/java/br/com/studies/listadesafios/ExercicioUm.java
@@ -0,0 +1,29 @@
+package br.com.studies.listadesafios;
+
+import java.util.Scanner;
+
+public class ExercicioUm {
+    public static void main(String[] args) {
+        Scanner dadosEntrada = new Scanner(System.in);
+        int numeroA, numeroB, numeroC;
+
+        System.out.println("Número A: ");
+        numeroA = dadosEntrada.nextInt();
+        System.out.println("Número B: ");
+        numeroB = dadosEntrada.nextInt();
+        System.out.println("Numero C: ");
+        numeroC = dadosEntrada.nextInt();
+
+        mostrarResultado(numeroA, numeroB, numeroC);
+
+    }
+
+
+    static void mostrarResultado(int numeroA, int numeroB, int numeroC){
+        int somaAeB = numeroA + numeroB;
+
+        String resultado = (somaAeB < numeroC) ? "A soma de A + B  eh menor" : "A soma de A + B nao eh menor que C";
+
+        System.out.println(resultado);
+    }
+}
