1584f065f6f4d369bd621ac85b1e112428ce3212
Joanice Oliveira <jhoaniceoliveira@gmail.com>
2024-07-31 22:12:05 -0300

    add - resolved exercise #4

---
+++ src/main/java/br/com/studies/listadesafios/ExercicioQuatro.java
@@ -0,0 +1,22 @@
+package br.com.studies.listadesafios;
+
+import java.util.Scanner;
+
+public class ExercicioQuatro {
+    public static void main(String[] args) {
+        Scanner dadosEntrada = new Scanner(System.in);
+
+        System.out.println("Digite um numero: ");
+        int numero = dadosEntrada.nextInt();
+
+        mostrarSucessorAntecessor(numero);
+    }
+
+    static void mostrarSucessorAntecessor(int numero){
+        int antecessor = (numero > 0) ? numero - 1: numero;
+        int sucessor = (numero > 0) ? numero + 1 : numero;
+
+        System.out.println("Antecessor do numero " + numero + ": " + antecessor);
+        System.out.println("Sucessor do numero " + numero + ": " + sucessor);
+    }
+}
