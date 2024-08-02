5bf9b5f8f2e3638bc56ddd6305830d85da1f9458
Joanice Oliveira <jhoaniceoliveira@gmail.com>
2024-08-01 22:04:07 -0300

    add - resolved exercise #8

---
+++ src/main/java/br/com/studies/listadesafios/ExercicioOito.java
@@ -0,0 +1,21 @@
+package br.com.studies.listadesafios;
+
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class ExercicioOito {
+
+    public static void main(String[] args) {
+        Scanner dadosEntrada = new Scanner(System.in);
+        int numero;
+        TreeSet<Integer> numeros = new TreeSet<>();
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Numero: ");
+            numero = dadosEntrada.nextInt();
+
+            numeros.add(numero);
+        }
+        numeros.forEach(System.out::println);
+    }
+}
