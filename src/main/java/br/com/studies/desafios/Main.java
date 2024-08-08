2be02ccf2aed3368b4c3869db7b170bf2af7ba57
Joanice Oliveira <jhoaniceoliveira@gmail.com>
2024-08-07 22:08:12 -0300

    add - resolved challenge #1

---
+++ src/main/java/br/com/studies/desafios/Main.java
@@ -0,0 +1,7 @@
+package br.com.studies.desafios;
+
+public class Main {
+    public static void main(String[] args) {
+        Perguntas.mostrarPerguntas();
+    }
+}
---
+++ src/main/java/br/com/studies/desafios/Pessoa.java
@@ -0,0 +1,4 @@
+package br.com.studies.desafios;
+
+public record Pessoa(String nome, String email, int idade, int altura) {
+}
