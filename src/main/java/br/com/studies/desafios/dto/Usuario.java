8c60ca1430a07284bf5d70bfd5aaa856c42e8936
Joanice Oliveira <jhoaniceoliveira@gmail.com>
2024-08-14 22:32:48 -0300

    add - added Hibernate validations in Usuario

--- src/main/java/br/com/studies/desafios/dto/Usuario.java
+++ src/main/java/br/com/studies/desafios/dto/Usuario.java
@@ -1,4 +1,18 @@
 package br.com.studies.desafios.dto;
 
-public record Usuario(String nome, String email, int idade, int altura) {
-}
+import jakarta.validation.constraints.*;
+
+public record Usuario(
+        @NotBlank(message = "O nome não pode ficar em branco")
+        @Size(min = 10, message = "O nome deve ter no minimo 10 caracteres")
+        String nome,
+
+        @NotBlank(message = "O preenchimento do email eh obrigatorio")
+        @Email(message = "Preencha com um email válido. Exemplo: email@gmail.com")
+        String email,
+
+        @Min(value = 18, message = "O usuario deve ter no minimo 18 anos")
+        int idade,
+
+        @Pattern(regexp = "\\d+,\\d+", message = "A altura deve estar no formato 'numero, virgula'")
+        String altura) { }
